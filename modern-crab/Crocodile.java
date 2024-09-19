270ffcd2103e549949ad59f1e35d7b5bf18f073b
Vlaicu Suba <vlaicu.suba@gmail.com>
2024-09-19 10:24:10 -0400

    first commit and added new enemy

---
+++ modern-crab/Crocodile.java
@@ -0,0 +1,57 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Crocodile extends Actor
+{
+
+    /**
+     * Act - do whatever the Crocodile wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        eatCrab();
+        moveAround();
+        eatLobster();
+    }
+
+    /**
+     * 
+     */
+    public void moveAround()
+    {
+        move(2);
+        if (Greenfoot.getRandomNumber(10) == 1) {
+            turn(Greenfoot.getRandomNumber(90) - 45);
+        }
+        if (isAtEdge()) {
+            turn(180);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void eatCrab()
+    {
+        if (isTouching(Crab.class)) {
+            World world = getWorld();
+            removeTouching(Crab.class);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void eatLobster()
+    {
+        if (isTouching(Lobster.class)) {
+            World world = getWorld();
+            removeTouching(Lobster.class);
+        }
+    }
+}
--- modern-crab/Lobster.java
+++ modern-crab/Lobster.java
@@ -22,7 +22,13 @@ public class Lobster extends Actor
      */
     public void act()
     {
+        startingDirection();
         moveAround();
+        crabDisappear();
+        removeWormAddLobster();
+        if (isGameLost()) {
+            transitionToGameOverWorld();
+        }
     }
 
     /**
@@ -36,6 +42,68 @@ public class Lobster extends Actor
         }
         if (isAtEdge()) {
             turn(180);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void startingDirection()
+    {
+        if (Greenfoot.getRandomNumber(10) == 1) {
+            turn(Greenfoot.getRandomNumber(360));
+        }
+    }
+
+    /**
+     * 
+     */
+    public void crabDisappear()
+    {
+        if (isTouching(Crab.class)) {
+            World world = getWorld();
+            removeTouching(Crab.class);
+            Greenfoot.playSound("scary.wav");
+        }
+    }
+
+    /**
+     * 
+     */
+    public boolean isGameLost()
+    {
+        World world = getWorld();
+        if (world.getObjects(Crab.class).isEmpty()) {
+            return true;
+            
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * 
+     */
+    public void transitionToGameOverWorld()
+    {
+        World gameOverWorld =  new  GameOverWorld();
+        Greenfoot.setWorld(gameOverWorld);
+    }
+
+    /**
+     * 
+     */
+    public void removeWormAddLobster()
+    {
+        Actor worm = getOneIntersectingObject(Worm.class);
+        if (worm != null) {
+            World world = getWorld();
+            int locationX = worm.getX();
+            int locationY = worm.getY();
+            Actor lobster =  new  Lobster();
+            world.removeObject(worm);
+            world.addObject(lobster, locationX, locationY);
         }
     }
 }
